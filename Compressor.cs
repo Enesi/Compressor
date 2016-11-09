1febc4ae2c49c98bf3cc9024751a6e7db52d29ec
Enesi <enesi@rambler.ru>
2016-11-10 00:19:30 +0200

    First commit
    Don't know, does it work and is it a correct way at all.
    But this is first try.

---
+++ Compressor.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Compression;
+
+namespace Compressor
+{
+    public struct Task 
+    {
+        public byte[] buffer;
+        public int numberOfPart;
+    }
+
+    public static class Compressor
+    {
+        static Thread[] threadPull;
+
+        static Queue<Task> allTasks; //или использовать потокобезопасную  ConcurrentQueue ?... 
+        static object allTasksLock;
+        static int partSize;
+        static int maxTasksCount;
+        static ManualResetEvent newTaskAdded; //если реализовывать специальную очередь, то событие будет в ее полномочиях
+        static ManualResetEvent needNewTask;
+
+        //или другая логика записи?
+        static List<Task> doneTasks; //или использовать потокобезопасную  ConcurrentQueue ?... 
+        static object doneTasksLock;
+        static ManualResetEvent newDoneTaskAdded;
+
+        //true - закончили считывать данные из файла
+        static bool endOfFile;
+
+        public static Compressor()
+        {
+            calculateSize(); //здесь?
+            endOfFile = false;
+            allTasks = new Queue<Task>();
+            doneTasks = new List<Task>();
+            threadPull = new Thread[maxTasksCount];
+            newTaskAdded = new ManualResetEvent(false);
+            needNewTask = new ManualResetEvent(true);
+            newDoneTaskAdded = new ManualResetEvent(false);
+        }
+
+        static void calculateSize()
+        {
+            //???? 
+            maxTasksCount = 5;
+            partSize = Environment.SystemPageSize/(maxTasksCount*2); 
+        }
+
+        public static void Compress(string source, string destination)
+        {
+            //это пока для простоты
+            source = "source.jpg";
+            destination = "out";
+
+            //проверки наличия файла?
+            using (FileStream sourceFile = new FileStream(source, FileMode.Open))
+            {
+                //содздаем и запускаем потоки для сжатия
+                for (int i = 0; i < maxTasksCount; i++)
+                    threadPull[i] = new Thread(consumerProcess);
+                foreach (Thread tr in threadPull)
+                    tr.Start();
+
+                Thread writeThread = new Thread(new ParameterizedThreadStart(writeProcess));
+                writeThread.Start(destination);
+
+                //читаем входной файл, генерируем задания и помещаем их в очередь
+                byte[] buffer  = new byte[partSize];
+                int partNumb = 0;
+                while (sourceFile.Read(buffer, 0, partSize) != 0)
+                {
+                    if (allTasks.Count==maxTasksCount)
+                        needNewTask.WaitOne();
+
+                    lock (allTasksLock)
+                    {
+                        Task newTask = new Task();
+                        newTask.buffer = buffer;
+                        newTask.numberOfPart = partNumb++;
+                        allTasks.Enqueue(newTask);
+                        newTaskAdded.Set();
+                    }
+                }
+                endOfFile = true;
+            }
+            foreach (var t in threadPull)
+                t.Join();
+        }
+
+        static void consumerProcess()
+        {
+            while (!endOfFile)
+            {
+                // если очередь заданий пуста, ждем события добавления нового задания
+                if (allTasks.Count == 0)
+                    newTaskAdded.WaitOne();
+
+                Task task;
+                lock (allTasksLock)
+                {
+                    task = allTasks.Dequeue();
+                    newTaskAdded.Reset();
+                }
+                Task doneTask = new Task();
+                doneTask.numberOfPart = task.numberOfPart;
+                using (MemoryStream output = new MemoryStream(partSize))
+                {
+                    using (GZipStream gzip = new GZipStream(output, CompressionMode.Compress))
+                    {
+                        gzip.Write(task.buffer, 0, partSize);
+                    }
+                    doneTask.buffer = output.ToArray();
+                }
+
+                lock (doneTasksLock)
+                {
+                    doneTasks.Add(doneTask);
+                    newDoneTaskAdded.Set();
+                }
+
+            }
+
+        }
+
+        static void writeProcess(object destination)
+        {
+            String fileName = destination.ToString();
+            if (File.Exists(fileName))
+                File.Delete(fileName);
+            using (FileStream resFile = new FileStream(fileName + ".gz", FileMode.Create, FileAccess.Write))
+            {
+                Task task;
+                int currPartNumb = 0;
+
+                while (!endOfFile) //еще надо проверять, работают ли еще потоки по сжатию
+                {
+                    //если очередь пуста, или в ней нет текущей на запись части, ждем
+                    if ((doneTasks.Count == 0)||(!doneTasks.Any(p=>p.numberOfPart==currPartNumb)))
+                        newDoneTaskAdded.WaitOne();
+ 
+                    lock (doneTasks)
+                    {
+                        task = doneTasks.Find(p=>p.numberOfPart==currPartNumb);
+                        doneTasks.Remove(task);
+                        newDoneTaskAdded.Reset();
+                    }
+                    resFile.Write(task.buffer, 0, task.buffer.Length);
+                    currPartNumb++;
+                }
+            }
+        }
+
+        public static void Decompress(string source, string destination)
+        {
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compressor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            switch (args[0])
+            {
+                case "compress":
+                    Compressor.Compress(args[1], args[2]);
+                    break;
+                case "decompress":
+                    Compressor.Decompress(args[1], args[2]);
+                    break;     
+                default:
+                    Console.WriteLine("incorrect command");
+                    break;
+            }
+        }
+    }
+}
